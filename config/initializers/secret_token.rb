3085d1ebaaf6eabfc043f58bdb0103d355fc566e
Edwin Vega <edwin.ponc@gmail.com>
2010-08-14 16:19:00 -0400

    Initialized commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapptest::Application.config.secret_token = 'a59ed0243212d642474450d4374e4819731eadcb459cabdf547c99fff028353db5e724cedc8ba7caa2086cd4d66c757ad248f2168372ceabb7f7bf05aaf71584'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Myapptest::Application.config.session_store :cookie_store, :key => '_myapptest_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Myapptest::Application.config.session_store :active_record_store
