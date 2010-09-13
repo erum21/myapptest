1f0c1d774aac8243ee7b0c602ec2e7ff0041591e
Edwin Vega <edwin.ponc@gmail.com>
2010-09-12 20:14:02 -0400

    Done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,17 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+unless defined?(Rails)
+	require File.dirname(__FILE__) + "/../config/environment"
+end
 require 'rspec/rails'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -24,4 +34,10 @@ RSpec.configure do |config|
   # examples within a transaction, comment the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  ActiveSupport::Dependencies.clear
+  end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs. 
 end
